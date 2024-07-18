cc3b93879bed04a47bccd42d6b9c0b3f38524d59
Jamal Mansur <jence@jence.com>
2024-07-18 11:21:29 +0600

    EJ

--- src/jence/swt/app/NfcApp.java
+++ src/jence/swt/app/NfcApp.java
@@ -39,7 +39,7 @@ import org.eclipse.swt.widgets.*;
 public class NfcApp {
 	
 	public static J4209N driver_ = new J4209N();
-	public static final String VERSION = "1.7";
+	public static final String VERSION = "1.8";
 	private static Shell app_;
 	private static Display display_;
 	
